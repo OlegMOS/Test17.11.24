63c775e748b4327f98920e2dce690cf832685272
OlegM <x2007@rambler.ru>
2024-11-25 00:13:27 +0500

    Scrapy_1

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["divan.ru"]
+    start_urls = ["https://www.divan.ru/chelyabinsk/category/podvesnye-svetilniki"]
+
+    def parse(self, response):
+        divans = response.css('div.LlPhw')
+        for divan in divans:
+            yield{
+                'name' : divan.css('div.lsooF span::text').get(),
+                'price' : divan.css('div.pY3d2 span::text').get(),
+                'url': response.urljoin(divan.css('a::attr(href)').get())
+            }
