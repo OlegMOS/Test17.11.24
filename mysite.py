0b0b4df7eb02f6966dc2419c122974722d017b6c
OlegM <x2007@rambler.ru>
2024-12-15 00:58:53 +0500

    Сайт с шаблоном_в1

---
+++ mysite.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    context = {
+        "link": "Перейти на главную страницу"
+    }
+    return render_template("home.html", **context)
+
+@app.route("/about.html")
+def about():
+    context = {
+        "link": "О нас"
+    }
+    return render_template("about.html", **context)
+
+if __name__ == "__main__":
+    app.run()
