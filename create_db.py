a94def87b0bff20a6b2ab1ac69dfcfde6419517d
OlegM <x2007@rambler.ru>
2024-12-22 21:06:56 +0500

    Seit_Login_del

--- create_db.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from app import db, app
-from app.models import User
-
-with app.app_context():  # Создаём контекст приложения
-    db.create_all()  # Создаёт базы данных и таблицы
